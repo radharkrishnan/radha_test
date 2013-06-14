8506d3ec02cb29969fa86a9039e74eb0dc1b637c
Radha Sreekanth <radha@alignminds.com>
2013-06-14 11:35:03 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadhaTest::Application.config.secret_token = '7af6d3813a91f2c8d7d5cbfe346bbace7f59153e0f3408f574ff1dc3ad074305fe9ce49b298869cba1cfdeaa71f0fd63a1df34f791759db19f56f3862bdebfae'
